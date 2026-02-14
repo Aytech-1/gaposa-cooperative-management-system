0908671724a361f1423fa5cf17a06f2712de58bc
ADEYEMI AYOBAMI SAMSON <adeyemiayobami273@gmail.com>
2026-02-14 11:22:45 +0100

    updated

--- routes/api.php
+++ routes/api.php
@@ -24,27 +24,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::prefix('admin')->group(function () {
-        Route::post('auth/login', [AdminAuthController::class, 'login'])->middleware('throttle:5,1');
-        Route::post('auth/verify-otp', [AdminAuthController::class, 'verifyOtp'])->middleware('throttle:5,1');
-        Route::post('auth/reset-password', [AdminAuthController::class, 'sendPasswordResetLink'])->middleware('throttle:5,1');
-        Route::post('auth/finish-reset-password', [AdminAuthController::class, 'finishResetPassword'])->middleware('throttle:5,1');
-        Route::post('auth/resend-password-reset-link', [AdminAuthController::class, 'resendPasswordResetLink'])->middleware('throttle:5,1');
+        
         Route::middleware(['auth:admin', 'trust.device'])->group(function () {
-            Route::post('auth/logout', [AdminAuthController::class, 'logout']);
-            Route::get('auth/profile', [AdminAuthController::class, 'fetchStaffProfile']);
-            Route::post('auth/change-password', [AdminAuthController::class, 'changePassword']);
             Route::apiResource('role', RoleController::class)->middleware('permission:manage role');
             Route::apiResource('activities', ActivitiesController::class)->only(['index', 'show'])->middleware('permission:view activities');
             Route::apiResource('staff', AdminController::class)->middleware('permission:manage staff');
             Route::apiResource('users', UserManagementController::class)->middleware('permission:manage users');
             Route::post('staff-passport/{id}', [StaffPassportController::class, 'update']);
 
-            Route::post('{staffId}/direct-permissions', [StaffPermissionController::class, 'assignDirectPermissions'])
-                ->middleware('permission:manage roles');
-            Route::delete(
-                '{staffId}/direct-permissions',
-                [StaffPermissionController::class, 'revokeDirectPermissions']
-            )->middleware('permission:manage roles');
+            
         });
 
         // Route::apiResource('role', RoleController::class);
